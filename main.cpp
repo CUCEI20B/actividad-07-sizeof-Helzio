f19c62ee11f16d0e6b2b228527ef0cf8aff3dd17
Oscar Mendez <oscar.mendez5121@gmai.com>
2020-09-29 10:12:19 -0500

    Prueba 1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string input;
+    cin >> input;
+
+    if (input == "char")
+    {
+        cout << sizeof(char) << endl;
+    } else if (input == "int")
+    {
+        cout << sizeof(int) << endl;
+    } else if (input == "double")
+    {
+        cout << sizeof(double) << endl;
+    } else if (input == "short")
+    {
+        cout << sizeof(short) << endl;
+    } else if (input == "float")
+    {
+        cout << sizeof(float) << endl;
+    } else if (input == "long")
+    {
+        cout << sizeof(long) << endl;
+    }
+    
+    
 
     return 0;
 }
